a6520f6b535160907ad6ec5eb4c5541c51b176f0
Nortin Powers <a.nortin@gmail.com>
2023-07-17 20:33:11 +0300

    add JwtTokenManager

---
+++ src/main/java/by/nortin/restjwt/token/JwtTokenManager.java
@@ -0,0 +1,65 @@
+package by.nortin.restjwt.token;
+
+import static by.nortin.restjwt.utils.Constants.ROLES;
+import static io.jsonwebtoken.SignatureAlgorithm.HS256;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.security.Key;
+import java.time.Duration;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.crypto.spec.SecretKeySpec;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtTokenManager {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.lifetime}")
+    private Duration jwtLifetime;
+
+    private final Key key = new SecretKeySpec(secret.getBytes(), SignatureAlgorithm.HS256.getJcaName());
+
+    public String generateJwtToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList();
+        claims.put(ROLES, roles);
+        Date issuedDate = new Date();
+        Date expiredDate = new Date(issuedDate.getTime() + jwtLifetime.toMillis());
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(issuedDate)
+                .setExpiration(expiredDate)
+                .signWith(key, HS256)
+                .compact();
+    }
+
+    public String getUserName(String token) {
+        return getAllClaimsFromToken(token).getSubject();
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<String> getUserRoles(String token) {
+        return getAllClaimsFromToken(token).get(ROLES, List.class);
+    }
+
+    private Claims getAllClaimsFromToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJwt(token)
+                .getBody();
+    }
+}
