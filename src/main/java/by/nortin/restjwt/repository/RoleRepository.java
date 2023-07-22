352a4beecc9fa5be735cca39dbf072a790a82bf7
Nortin Powers <a.nortin@gmail.com>
2023-07-22 20:51:11 +0300

    added RoleRepository

--- src/main/java/by/nortin/restjwt/repository/RoleRepository.java
+++ src/main/java/by/nortin/restjwt/repository/RoleRepository.java
@@ -2,7 +2,9 @@ package by.nortin.restjwt.repository;
 
 import by.nortin.restjwt.domain.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
 }
