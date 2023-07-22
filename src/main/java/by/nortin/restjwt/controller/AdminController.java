53418a47d70969229a92336ac691d91454174370
Nortin Powers <a.nortin@gmail.com>
2023-07-22 20:46:06 +0300

    added controllers

---
+++ src/main/java/by/nortin/restjwt/controller/AdminController.java
@@ -0,0 +1,10 @@
+package by.nortin.restjwt.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+}
