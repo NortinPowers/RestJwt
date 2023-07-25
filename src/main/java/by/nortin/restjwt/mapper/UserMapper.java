38c7763a770dc9819c1faede7919890171416ac5
Nortin Powers <a.nortin@gmail.com>
2023-07-25 19:32:48 +0300

    updated mapper

--- src/main/java/by/nortin/restjwt/mapper/UserMapper.java
+++ src/main/java/by/nortin/restjwt/mapper/UserMapper.java
@@ -2,18 +2,20 @@ package by.nortin.restjwt.mapper;
 
 import by.nortin.restjwt.domain.User;
 import by.nortin.restjwt.dto.UserDto;
+import by.nortin.restjwt.dto.UserRegistrationDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE, uses = BaseMapper.class)
 public interface UserMapper {
 
-//    @Mapping(target = "role", source = "user.role.name")
+    @Mapping(target = "role", source = "user.role.name")
     UserDto convertToDto(User user);
 
 //    @InheritInverseConfiguration
 //    @Mapping(target = "role", expression = "java(new Role(dto.getRole()))")
-    User convertToDomain(UserDto userDto);
+//    User convertToDomain(UserDto userDto);
 
-//    User convertToDomain(UserRegistrationDto userRegistrationDto);
+    User convertToDomain(UserRegistrationDto userRegistrationDto);
 }
